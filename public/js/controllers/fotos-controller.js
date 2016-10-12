c0eeaa38cfed0a3499f3945bad9a9295eff1fa7e
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-10-12 12:51:11 -0300

    Tornando nossa aplicação mais próxima de uma aplicação real

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,11 @@
-angular.module('alurapic').controller('FotosController', function($scope){
+angular.module('alurapic').controller('FotosController', function($scope, $http){
 	
-	$scope.foto = {
-			titulo : 'Leão',
-			url : 'http://www.fundosanimais.com/Minis/leoes.jpg',
-	};
+	$scope.fotos = [];
 	
+	$http.get('v1/fotos').success(function(fotos){
+		$scope.fotos = fotos;
+	}).error(function(error){
+		console.log(error);
+	}); 
+
 });
