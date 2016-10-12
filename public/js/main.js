2cdbb7a9bd799115ddb5526b1ce8aade0323de94
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-10-12 16:24:14 -0300

    Dividir para conquistar!

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute']).config(function($routeProvider, $locationProvider){
+	
+	$locationProvider.html5Mode(true);
+	
+	$routeProvider.when('/fotos', {
+		templateUrl: 'partials/principal.html',
+		controller: 'FotosController',
+	});
+	
+	$routeProvider.when('/fotos/new', {
+		templateUrl: 'partials/foto.html',
+	});
+
+	$routeProvider.otherwise({
+		redirectTo: '/fotos',
+	})
+
+});
