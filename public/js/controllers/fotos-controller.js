c3d6c96cb63c1871580398d61754a6ad203ad959
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-10-12 15:51:49 -0300

    Melhorando a experiência do usuário

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,7 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
 	
 	$scope.fotos = [];
+	$scope.filtro = '';
 	
 	$http.get('v1/fotos').success(function(fotos){
 		$scope.fotos = fotos;
