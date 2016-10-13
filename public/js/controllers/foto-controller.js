f79f8a12c6e520dfe16c4af268b27353ebfaf15f
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-10-12 22:45:40 -0300

    7recisamos alimentar esse sistema - part2

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,18 @@
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http){
 	
 	$scope.foto = {};
+	$scope.mensagem = '';
 	
 	$scope.submeter = function() {
-		console.log($scope.foto);
+		if($scope.formulario.$valid) {
+			$http.post('v1/fotos', $scope.foto).success(function(){
+				$scope.foto = {};
+				$scope.mensagem = 'Foto incluida com sucesso!';
+			}).error(function(erro){
+				$scope.mensagem = 'Não foi possivel incluir a foto!';
+				console.log(erro);
+			});
+		}
 	};
 	
 });
